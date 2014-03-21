8d57f36604f4a99180ab3e8d0fbe49591ca534d4
Ryan Brown <brown.ry@husky.neu.edu>
2014-03-21 11:23:22 -0400

    Added encode/decode texts for saving future texts when server dies

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import request
 from flask import send_from_directory
-import os
 from time import sleep
 import datetime
 import thread
@@ -32,36 +31,38 @@ def text():
 
     date = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute)
     texts.append(Text(to, provider, message, date))
-  
-    #with open('log.txt', 'a') as f:
-    #    ip = str(request.remote_addr)
-    #    t = str(datetime.datetime.now())
-    #    logtext = ' | '.join([ip, t, to, provider, message]) + '\n'
-    #    f.write(logtext)
 
-    return htmltop + 'Sent text to ' + to + ' with provider '+ provider + ' and message: ' + message + '<br/><a href="/">Back</a>' + htmlbot
+    return htmltop + 'Success!<br/><a href="/">Back</a>' + htmlbot
 
 def text_runner(delay):
     global texts
     
     sendtexts = [text for text in texts if text.ready() == True]
     texts = [text for text in texts if text not in sendtexts]
-    
+
+    f = open('texts', 'w')
+    for text in texts:
+        f.write(text.encode()+'\n')
+    f.close()
+
     for text in sendtexts:
         textr.send(text)
     
     sleep(delay)
     text_runner(delay)
 
-#@app.route('/time')
-#def timeleft():
-#    t = datetime.datetime(2014, 3, 18, 17, 30) - datetime.datetime.now()
-#    return 'Only '+str(t.seconds)+' seconds left!! <3'
-
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(app.root_path, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+def load_texts():
+    f = open('texts', 'r')        
+    lines = f.readlines()
+    for line in lines:
+        texts.append(Text.decode(line))
+    f.close()
+
 if __name__ == '__main__':
+    load_texts()
     thread.start_new_thread(text_runner, (15,))
     app.run('0.0.0.0', 80)
